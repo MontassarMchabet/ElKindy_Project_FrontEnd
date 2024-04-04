8d3d81705d7bc1aa270794490e03d9b490dde8bc
RahmaSlimi <rahmaaslimii83@gmail.com>
2024-04-04 14:26:23 +0100

    Updates

--- Elkindy-Frontend-Frontoffice/src/components/Events/EventsDetail/EventCommentForm.jsx
+++ Elkindy-Frontend-Frontoffice/src/components/Events/EventsDetail/EventCommentForm.jsx
@@ -28,19 +28,65 @@
 // };
 
 // export default EventCommentForm;
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { addComment } from "../../../services/eventsCommentApi"; 
 import { useParams } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
+import Cookies from "js-cookie";
 
 const EventCommentForm = () => {
+  
   const { eventId } = useParams(); // Récupérez l'identifiant de l'événement à partir des paramètres d'URL
   const [message, setMessage] = useState("");
+  const [userId, setUserId] = useState(null);
 
+  useEffect(() => {
+      const getUserIdFromToken = () => {
+          const storedToken = Cookies.get('token');
+          if (storedToken) {
+              const decodedToken = jwtDecode(storedToken);
+              const userId = decodedToken.userId;
+              setUserId(userId);
+          }
+      };
+
+      getUserIdFromToken();
+  }, []);
+  
+  console.log('userrrrrrr',userId)
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   try {
+  //     // Données du commentaire à envoyer
+  //     const commentData = {
+  //       userId: userId, // Incluez l'ID de l'utilisateur connecté dans les données du commentaire
+  //       comment: message,
+  //       // Ajoutez d'autres données de commentaire si nécessaire
+  //     };
+  //     const config = {
+  //       // Ajoutez des options de configuration si nécessaire
+  //     };
+  
+  //     // Envoyez le commentaire au backend pour être associé à l'événement
+  //     await addComment(eventId, commentData, config);
+  
+  //     // Réinitialisez le champ de texte après l'envoi du commentaire
+  //     setMessage("");
+  
+  //     // Vous pouvez également ajouter une logique pour actualiser la liste des commentaires après l'ajout du nouveau commentaire si nécessaire
+  //   } catch (error) {
+  //     console.error("Error sending comment :", error);
+  //     // Ajoutez une logique pour afficher un message d'erreur à l'utilisateur si nécessaire
+  //   }
+  // };
+  
+
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       // Envoyez le commentaire au backend pour être associé à l'événement
-      await addComment(eventId, { comment: message });
+      await addComment(eventId, { comment: message, user: userId });
       // Réinitialisez le champ de texte après l'envoi du commentaire
       setMessage("");
       // Vous pouvez également ajouter une logique pour actualiser la liste des commentaires après l'ajout du nouveau commentaire si nécessaire
@@ -49,6 +95,10 @@ const EventCommentForm = () => {
       // Ajoutez une logique pour afficher un message d'erreur à l'utilisateur si nécessaire
     }
   };
+  
+
+ 
+
 
   const handleChange = (e) => {
     setMessage(e.target.value);
