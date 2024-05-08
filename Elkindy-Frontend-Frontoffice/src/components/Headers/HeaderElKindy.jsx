476453a0c620860ce27d8be39fe12ca2451cf1da
Montassar Mchabet <100842319+MontassarMchabet@users.noreply.github.com>
2024-05-08 23:41:53 +0100

    Update HeaderElKindy.jsx front2

--- Elkindy-Frontend-Frontoffice/src/components/Headers/HeaderElKindy.jsx
+++ Elkindy-Frontend-Frontoffice/src/components/Headers/HeaderElKindy.jsx
@@ -464,7 +464,7 @@ const HeaderOne = () => {
                                                         }}>
                                                             <a
                                                                 style={{ color: "#6c757d", textDecoration: "none", display: "inline-block", width: "100%", height: "100%", lineHeight: "40px" }}
-                                                                href="el-kindy-project-front-end-t52l.vercel.app/elkindy#/signin"
+                                                                href="https://el-kindy-project-front-end-t52l.vercel.app/elkindy#/signin"
                                                             >Sign in</a>
                                                         </button>
                                                         <button style={{
